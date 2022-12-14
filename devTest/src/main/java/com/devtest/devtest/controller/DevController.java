4670a957a40b637739ae2b45fb4dbb61a5ec8259
Hoonchangyi <andy9912@naver.com>
2022-12-14 17:40:45 +0900

    221214 json파싱 후 검색 구현 #8

--- devTest/src/main/java/com/devtest/devtest/controller/DevController.java
+++ devTest/src/main/java/com/devtest/devtest/controller/DevController.java
@@ -1,6 +1,8 @@
 package com.devtest.devtest.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ public class DevController {
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(value = "/home")
         public String Hello(){
-            return "연결성공";
+
+        return "연결성공";
     }
 }
--- devTest/src/main/test_dev/src/js/Main.js
+++ devTest/src/main/test_dev/src/js/Main.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Footer from './Form/Footer';
 import Header from './Form/Header';
 import axios from "axios";
+import news from "./json/news.json"
 import "./Main.css";
 
 function Main() {
@@ -10,9 +11,12 @@ function Main() {
             <Header />
             <MainContainer></MainContainer>
         </div>
-        
+
     )
 }
+
+// console.log(news);
+
 
 function MainContainer() {
     const [message, setMessage] = useState('');
@@ -34,39 +38,100 @@ function MainContainer() {
     const [isLogin, setIsLogin] = useState(false);
 
     useEffect(() => {
-      if (sessionStorage.getItem("tokenId") === null) {
-         // sessionStorage 에 name 라는 key 값으로 저장된 값이 없다면
-         console.log("isLogin ?? :: ", isLogin);
-      } else {
-         // sessionStorage 에 name 라는 key 값으로 저장된 값이 있다면
-         // 로그인 상태 변경
-         setIsLogin(true);
-         console.log("isLogin ?? :: ", isLogin);
-      }
-   });
+        if (sessionStorage.getItem("tokenId") === null) {
+            // sessionStorage 에 name 라는 key 값으로 저장된 값이 없다면
+            console.log("isLogin ?? :: ", isLogin);
+        } else {
+            // sessionStorage 에 name 라는 key 값으로 저장된 값이 있다면
+            // 로그인 상태 변경
+            setIsLogin(true);
+            console.log("isLogin ?? :: ", isLogin);
+        }
+    });
+    const [search, setSearch] = useState('');
+
+    const styleInfo = {
+        paddingRight: '7px'
+    }
+
+    const jlink = (link, e) => {
+        console.log(link);
+    
+        if (link != null) {
+            const jobKorea = "https://www.jobkorea.co.kr" + link;
+            window.open(jobKorea);
+        } else {
+            window.location.href = "/";
+        }
+    }
+
+    const items = news.Information.filter((data) => {
+        if (search == "") {
+            return null
+        }
+        else if (data.name.toLowerCase().includes(search.toLowerCase()) || data.name.toLowerCase().includes(search.toLowerCase())) {
+            return data
+        }
+    }).map((data, key) => {
+        return (
+        <ItemBox name={data.name} state={data.state} content={data.content} position={data.position} plan={data.plan} link={data.link}/>
+        )
+    })
+
 
     return (
         <div className="banner_box">
 
-        <img className="bannerImg" alt="banner_01" src="img/job_banner.PNG" />
+            <img className="bannerImg" alt="banner_01" src="img/job_banner.PNG" />
 
             <br></br>
-        <span class="banner_text">원하는 회사의 정보를 얻어가세요!</span>
-        <span class="banner_text2">Find You Want Company</span>
-        <br></br>
+            <span className="banner_text">원하는 회사의 정보를 얻어가세요!</span>
+            <span className="banner_text2">Find You Want Company</span>
+            <br></br>
 
-        <div class="serach_input">
-			<input type="text" placeholder="회사를 입력하세요"/>
-			<button class="search_submit">
-				<i class="fa-solid fa-magnifying-glass"></i>
-			</button>
-		</div>
+            <div className="serach_input">
+                <input type="text" placeholder="회사를 입력하세요" onChange={event => { setSearch(event.target.value) }} />
+                <button className="search_submit">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </div>
 
-            
+            {items}
+
         </div>
+
     )
+
+
+
+    function ItemBox(props) {
+        return (
+            <div className="MainBox" onClick={(e) => {jlink(props.link)}}>
+                <div className  ="MainName">
+                    <span>{props.name}</span>
+                    <br/>
+                    <span>{props.state}</span>
+                    <br/>
+                    <span>{props.content}</span>
+                    <br/>
+                    <span>{props.position}</span>
+                    <br/>
+                    <span>{props.plan}</span>
+                </div>
+            </div>
+        )
+    }
+
 
 
 }
 
 export default Main;
+
+
+
+
+
+
+
+
